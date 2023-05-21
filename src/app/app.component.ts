053dd9e68c5d0c7f2e0e653e7f015d75267b65c6
DrozdovDenys <denysjsjr@gmail.com>
2023-05-21 18:32:52 +0300

    created porduct card, added services to get all product and handle errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductService } from './services/product.services';
+import { Observable, tap } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-my-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-angular-app';
+export class AppComponent implements OnInit {
+	title = 'my angular app';
+	// products: IProduct[] = [];
+	products$: Observable<IProduct[]>;
+	loading = false;
+
+	constructor(private productService: ProductService) {}
+
+	ngOnInit(): void {
+		this.loading = true;
+
+		this.products$ = this.productService
+			.getAll()
+			.pipe(tap(() => (this.loading = false)));
+		// this.productService.getAll().subscribe((products) => {
+		// 	this.products = products;
+		// 	this.loading = false;
+		// });
+	}
 }
